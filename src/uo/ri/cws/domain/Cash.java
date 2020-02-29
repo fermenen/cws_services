673ee70a64e200adc4ee33e0af3d872cdbe40796
Fernando Menendez <f.menendez@betterplaceapp.com>
2020-02-29 18:08:35 +0100

    remove unnecessary type field

--- src/uo/ri/cws/domain/Cash.java
+++ src/uo/ri/cws/domain/Cash.java
@@ -8,52 +8,52 @@ import javax.persistence.*;
 @Table(name = "TCASHES")
 public class Cash extends PaymentMean {
 
-    public Cash() {
-	super("Cash");
-    }
+	public Cash() {
 
-    public Cash(Client cliente) {
-	this();
-	Associations.Pay.link(this, cliente);
-    }
+	}
 
-    public double getAccumulated() {
+	public Cash(Client cliente) {
+		this();
+		Associations.Pay.link(this, cliente);
+	}
 
-	return super.getAcumulado();
-    }
+	public double getAccumulated() {
 
-    protected Set<Charge> _getCharges() {
-	return super._getCharges();
-    }
+		return super.getAcumulado();
+	}
 
-    @Override
-    public String toString() {
-	return "Metalico [toString()=" + super.toString() + "]";
-    }
+	protected Set<Charge> _getCharges() {
+		return super._getCharges();
+	}
 
-    @Override
-    public int hashCode() {
-	final int prime = 31;
-	int result = 1;
-	result = prime * result + ((client == null) ? 0 : client.hashCode());
-	return result;
-    }
+	@Override
+	public String toString() {
+		return "Metalico [toString()=" + super.toString() + "]";
+	}
 
-    @Override
-    public boolean equals(Object obj) {
-	if (this == obj)
-	    return true;
-	if (obj == null)
-	    return false;
-	if (getClass() != obj.getClass())
-	    return false;
-	PaymentMean other = (PaymentMean) obj;
-	if (client == null) {
-	    if (other.client != null)
-		return false;
-	} else if (!client.equals(other.client))
-	    return false;
-	return true;
-    }
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((client == null) ? 0 : client.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		PaymentMean other = (PaymentMean) obj;
+		if (client == null) {
+			if (other.client != null)
+				return false;
+		} else if (!client.equals(other.client))
+			return false;
+		return true;
+	}
 
 }
--- src/uo/ri/cws/domain/CreditCard.java
+++ src/uo/ri/cws/domain/CreditCard.java
@@ -6,78 +6,78 @@ import javax.persistence.*;
 @Entity
 @Table(name = "TCREDITCARDS")
 public class CreditCard extends PaymentMean {
-    @Column(unique = true)
-    private String number;
-    private String type;
-    private Date validThru;
+	@Column(unique = true)
+	private String number;
+	private String type;
+	private Date validThru;
 
-    public CreditCard() {
-	super("CreditCard");
-    }
+	public CreditCard() {
+		super();
+	}
 
-    public CreditCard(String numero) {
-	this();
-	this.number = numero;
-    }
+	public CreditCard(String numero) {
+		this();
+		this.number = numero;
+	}
 
-    public CreditCard(String numero, String tipo, Date validez) {
-	this(numero);
-	this.type = tipo;
-	this.validThru = validez;
-    }
+	public CreditCard(String numero, String tipo, Date validez) {
+		this(numero);
+		this.type = tipo;
+		this.validThru = validez;
+	}
 
-    public String getNumber() {
-	return number;
-    }
+	public String getNumber() {
+		return number;
+	}
 
-    public String getType() {
-	return type;
-    }
+	public String getType() {
+		return type;
+	}
 
-    public Date getValidThru() {
-	return validThru;
-    }
+	public Date getValidThru() {
+		return validThru;
+	}
 
-    public double getAccumulated() {
+	public double getAccumulated() {
 
-	return super.getAcumulado();
-    }
+		return super.getAcumulado();
+	}
 
-    @Override
-    public void pay(double importe) {
-	if (new Date().after(validThru))
-	    throw new IllegalStateException("No se puede usar esta tarjeta");
+	@Override
+	public void pay(double importe) {
+		if (new Date().after(validThru))
+			throw new IllegalStateException("No se puede usar esta tarjeta");
 
-	super.pay(importe);
-    }
+		super.pay(importe);
+	}
 
-    @Override
-    public int hashCode() {
-	final int prime = 31;
-	int result = 1;
-	result = prime * result + ((number == null) ? 0 : number.hashCode());
-	return result;
-    }
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((number == null) ? 0 : number.hashCode());
+		return result;
+	}
 
-    @Override
-    public boolean equals(Object obj) {
-	if (this == obj)
-	    return true;
-	if (obj == null)
-	    return false;
-	if (getClass() != obj.getClass())
-	    return false;
-	CreditCard other = (CreditCard) obj;
-	if (number == null) {
-	    if (other.number != null)
-		return false;
-	} else if (!number.equals(other.number))
-	    return false;
-	return true;
-    }
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		CreditCard other = (CreditCard) obj;
+		if (number == null) {
+			if (other.number != null)
+				return false;
+		} else if (!number.equals(other.number))
+			return false;
+		return true;
+	}
 
-    @Override
-    public String toString() {
-	return "TarjetaCredito [numero=" + number + "]";
-    }
+	@Override
+	public String toString() {
+		return "TarjetaCredito [numero=" + number + "]";
+	}
 }
